d4e873fee8b6369afea5c61a7f7539bcfdeea5f3
Mustafa Magdy <45540037+Mustafa-Magdyy@users.noreply.github.com>
2018-12-21 17:56:29 +0200

    Update UserControler.java

--- UserControler.java
+++ UserControler.java
@@ -1,5 +1,23 @@
 package lostAndFound;
 
-public class UserControler {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class UserController {
+	private UserView view;
+	private UserModel model;
+	
+	public UserController(UserView view, UserModel model) {
+		this.view = view;
+		this.model = model;
+		
+		view.addSaveListener(new saveListener());
+	}
+	
+	public class saveListener implements ActionListener{
+		public void actionPerformed(ActionEvent click) {
+			// save changes to user model and save changes to database
+			System.out.println(view.getName() + "\n" + view.getUserName() + "\n" + view.getEmail());
+		}
+	}
 }
