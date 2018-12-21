f1a0de526ec67f34b1c5ce23df6fcf9acfbe1189
Mustafa Magdy <45540037+Mustafa-Magdyy@users.noreply.github.com>
2018-12-21 15:41:34 +0200

    Update lostAndFoundMVC.java

--- lostAndFoundMVC.java
+++ lostAndFoundMVC.java
@@ -1,7 +1,13 @@
 package lostAndFound;
 
+import javax.swing.SwingUtilities;
+
 public class lostAndFoundMVC {
 	public static void main(String[] args) {
-		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new LoginView().setVisible(true);
+			}
+		});
 	}
 }
