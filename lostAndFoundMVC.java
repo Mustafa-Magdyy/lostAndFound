2a3a869ae76b679d144b6d05d009c1156f322643
Mustafa Magdy <45540037+Mustafa-Magdyy@users.noreply.github.com>
2018-12-23 23:27:18 +0200

    Update lostAndFoundMVC.java

--- lostAndFoundMVC.java
+++ lostAndFoundMVC.java
@@ -7,18 +7,20 @@ public class lostAndFoundMVC {
 	public static void main(String[] args) {
 		UserModel owner = new UserModel("Mustafa", "Mustafa_Magdy", "+201061393805", "dar41997@gamil.com", "180613059", 22);
 		UserView ownerView = new UserView(owner);
-		UserController ownerController = new UserController(ownerView, owner);
+		UserItemEnteringView uiev = new UserItemEnteringView(owner);
+		UserController ownerController = new UserController(owner, ownerView, uiev);
+		curUser = ownerController;
 		
-		ItemModel im = new ItemModel(ownerController, "mobile", "samsung Note 3", "Giza", "what is the phone password ??", null);
+		ItemModel im = new ItemModel(ownerController, "mobile", "samsung Note 3", "Giza", "what is the phone password ??");
 		ItemOwnerView iov = new ItemOwnerView(im);
 		ItemUserView iuv = new ItemUserView(im);
 		ItemSecurityView isv = new ItemSecurityView(im);
 		ItemResponseView irv = new ItemResponseView(im);
-		ItemController ic = new ItemController(iov, iuv, isv, irv, im);
+		ItemController ic = new ItemController(im, iov, iuv, isv, irv);
 		
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				irv.setVisible(true);
+				uiev.setVisible(true);
 			}
 		});
 	}
